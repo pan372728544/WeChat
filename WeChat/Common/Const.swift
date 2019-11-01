c8e1b22be41817b61d1fb31cc977ecb3f64aaedf
panzhijun <panzhijun@gomeplus.com>
2019-11-01 10:29:42 +0800

    默认使用白色模式

--- WeChat/Common/Const.swift
+++ WeChat/Common/Const.swift
@@ -27,7 +27,7 @@ let NavaBar_H : CGFloat = StatusBar_H + 44
 
 
 var isConnected = false
-let socketClient : ZJSocket = ZJSocket(addr: "10.2.116.47", port: 9999)
+let socketClient : ZJSocket = ZJSocket(addr: "10.2.116.45", port: 9999)
 
 
 let MeCellH : CGFloat = 56
